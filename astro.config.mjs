c1b08a79523d808568fe05a640bd4a5aa70046ca
RogerCiv <rogercivdev@gmail.com>
2025-12-26 12:39:48 +0100

    feat: update site configuration, add sitemap integration, and enhance SEO metadata for improved visibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tu-dominio.com',
-
+  site: 'https://web-recursos.vercel.app', // Cambia esto por tu dominio real
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   },
-
   adapter: vercel()
 });
